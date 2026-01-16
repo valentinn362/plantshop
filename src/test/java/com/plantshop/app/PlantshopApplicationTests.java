62949a37db3f145e0eebe0a28e87c64bf57e1dca
valentin362 <alvezrojasvalentin@gmail.com>
2026-01-16 01:16:17 -0300

    Initial commit: Spring Boot Planshop project

---
+++ src/test/java/com/plantshop/app/PlantshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantshop.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
