1bf4cb3eb6d1928051aea4d0f4d33ef7574de8e5
valentin362 <alvezrojasvalentin@gmail.com>
2026-02-01 12:42:38 -0300

    fix

--- src/main/java/com/plantshop/app/enums/TipoProducto.java
+++ src/main/java/com/plantshop/app/enums/TipoProducto.java
@@ -2,13 +2,8 @@ package com.plantshop.app.enums;
 
 public enum TipoProducto {
     PLANTA,
-    SUSTRATO_TIERRA,
-    FERTILIZANTE,
-    DECORACION,
     MACETA,
-    HERRAMIENTA,
-    ACCESORIO,
-    SEMILLA,
-    ILUMINACION,
-    ARBOL
+    DECORACION,
+    ARTICULO_JARDINERIA,
+    AROMAS
 }
